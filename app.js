de15cc64bd0ed954c3fb7d07d25dafe6c569a5cf
Shaidous <86616360+Shaidous@users.noreply.github.com>
2022-06-30 19:48:40 +0300

    Update app.js

--- app.js
+++ app.js
@@ -70,11 +70,3 @@ btn.on('click', function(e) {
 e.preventDefault();
 $('html, body').animate({scrollTop:0}, '300');
 });
-
-/* Костыль для мобилок */
-if (window.devicePixelRatio !== 1) { // Костыль для определения иных устройств, с коэффициентом отличным от 1		
-  var dpt = window.devicePixelRatio;
-  var widthM = window.screen.width * dpt;
-  var widthH = window.screen.height * dpt;
-  document.write('<meta name="viewport" content="width=' + widthM+ ', height=' + widthH + '">');
-}
